f30bed83df6b703896a8506537fa1de5fd914e0e
Kasidit0052 <61010052@kmitl.ac.th>
2020-12-19 16:34:21 +0700

    addSave&SendPoint

--- scripts/server.js
+++ scripts/server.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 ////////// ROSLIBJS Init
 var ROSLIB = require('roslib');
+const { json } = require('body-parser');
 var ros = new ROSLIB.Ros();
 ////////// HTTP Configurations
 app.use(bodyParser.json());
@@ -17,19 +18,19 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-
+////////// Current Directory
+const current_path = process.cwd();
 
 ////////// Set connect Interval for ROS
 
 // Attach function
 function init_ros() {
-  ros.connect('ws://172.16.10.20:9090');
+  //ros.connect('ws://172.16.10.20:9090');
   if(ros.isConnected){global.rosIsConnected = true;}
   else{global.rosIsConnected = false;}
 }
 // Set Interval
-intervalid = setInterval(init_ros,1000);
+intervalid = setInterval(init_ros,100);
 
 //////////
 
@@ -142,8 +143,8 @@ app.get('/api/getConnection', (req, res) => {
   res.send(JSON.stringify(global.rosIsConnected));
 });
 
-// MobileRobot ActionClient (usages=> {"CoordinateX" : 2.5 , "CoordinateY" ; 3.0})
-app.post('/api/coordinate', (req, res) => {
+// MobileRobot Movebase using Coordinate (usages=> {"CoordinateX" : 2.5 , "CoordinateY" ; 3.0})
+app.post('/api/moveBaseCoordinate', (req, res) => {
   // Initializing Goal 
   var positionVec3 = new ROSLIB.Vector3(null);
   var orientation = new ROSLIB.Quaternion({x:0, y:0, z:0, w:1.0});
@@ -190,8 +191,165 @@ app.post('/api/robot_cancel', (req, res) => {
   actionClient.cancel();
 });
 
+// Receive Point and save as Json File
+// Usages 
+// {
+//   "name":"Robotic Lab",
+//   "poseMessage":
+//   {
+//       "targetPose":
+//       {
+//           "header":{"frame_id":"map"},
+//           "Pose":
+//           {
+//               "position":{"x":"0.2","y":"0.1","z":"0.3"},
+//               "orientation":{"x":"1","y":"2","z":"3","w":"4"}
+//           }
+//       }
+//   }
+// }
+app.post('/api/savePoint', (req, res) => {
+
+  // location name variable
+  const location_name = req.body.name;
+
+  // location poseMessage variable
+  const location_poseMessage = req.body.poseMessage;
+
+  // path to  persistent_database
+  const filepath = path.join(current_path,'/scripts/persistent_data.txt');           
+  
+  // check if files exist or not
+  if(fs.existsSync(filepath)){
+
+      // check json structure
+      if( typeof location_name != "undefined" &&  typeof location_poseMessage != "undefined")
+      {
+        // read json file
+        fs.readFile(filepath, 'utf8', function (err,data) {
+          if (err) throw err;
+          obj = JSON.parse(data);
+          // append new point to an existing json object
+          obj.push(req.body);
+
+          // rewrite the json file
+          fs.writeFile(filepath, JSON.stringify(obj) , function (err) {
+            if (err) throw err;
+            res.json('File writed successfully.');
+          });
+        });
+      } 
+      else
+      {
+        res.json("wrong format"); 
+      } 
+  }
+  else{
+      // Initialize Json file
+      fs.writeFile(filepath, '[]', function (err) {
+        if (err) throw err;
+      });
+
+      // check json structure
+      if( typeof location_name != "undefined" &&  typeof location_poseMessage != "undefined")
+      {
+        // read json file
+        fs.readFile(filepath, 'utf8', function (err,data) {
+          if (err) throw err;
+          obj = JSON.parse(data);
+          // append new point to an existing json object
+          obj.push(req.body);
+
+          // rewrite the json file
+          fs.writeFile(filepath, JSON.stringify(obj) , function (err) {
+            if (err) throw err;
+            res.json('File writed successfully.');
+          });
+        });
+      } 
+      else
+      {
+        res.json("wrong format"); 
+      } 
+  }
+});
+
+// Send lists of location
+app.get('/api/loadPoint', (req, res) => {
+
+  // path to  persistent_database
+  const filepath = path.join(current_path,'/scripts/persistent_data.txt'); 
+
+  // Check whether file is exist or not
+  if(fs.existsSync(filepath)){
+    // read and return location lists
+    fs.readFile(filepath, 'utf8', function (err,data) {
+      if (err) throw err;
+      obj = JSON.parse(data);
+
+      var response_array = [];
+      for(var key in obj){response_array.push(obj[key].name);}
+
+      res.json(response_array);
+    });
+  }
+  else{
+    res.json("no point to load");
+  }
+});
+
+// MobileRobot Movebase using Location (usages=> {"index" : key_value)
+app.post('/api/moveBasePoint', (req, res) => {
+
+  // path to  persistent_database
+  const filepath = path.join(current_path,'/scripts/persistent_data.txt'); 
+
+  // Check whether file is exist or not
+  if(fs.existsSync(filepath))
+  {
+    // read and Instaitiate Actionlib Goal
+    fs.readFile(filepath, 'utf8', function (err,data) {
+
+      // throw error 
+      if (err) throw err;
+
+      // load JSON string to javascript object
+      obj = JSON.parse(data);
+
+      // Initializing Goal 
+      var goal = new ROSLIB.Goal({
+          actionClient : actionClient,
+          goalMessage : {target_pose : 
+            obj[parseInt(req.body.index)].poseMessage.targetPose
+          }
+      });
+
+      // Send Goal
+      goal.send();
+
+      // Check for Feedback
+      goal.on('feedback',function(feedback)
+      {
+        //console.log(feedback);
+      });
+
+      // Check for result and stop actionclient
+      goal.on('result',function(result)
+      {
+        actionClient.cancel();
+        //console.log(result);
+      });
+
+      res.json("robot move sucessfully");
+    });
+  }
+  else{
+    res.json("no point to load");
+  }
+});
+
 // get Map lists
-app.get('/admin',(req, res) => {
+app.get('/api/getMapList',(req, res) => {
   var EXTENSION = '.yaml';
   var dirPath = '/home/parallels/map';
   fs.readdir(dirPath, function(err, files){
@@ -268,8 +426,6 @@ app.post('/api/deleteMap', (req, res) => {
   });
 });
 //////////
-
-
 
 ////////// Specific listening port
 app.listen(port, () => console.log(`Listening on port ${port}`));
