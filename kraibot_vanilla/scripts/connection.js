968c2d245ca9a1a42ba718cec0e9553e589d7bb4
Parallels <tonydude1999@gmail.com>
2020-12-27 18:05:43 +0700

    add blocking modal and nav condition

--- kraibot_vanilla/scripts/connection.js
+++ kraibot_vanilla/scripts/connection.js
@@ -6,6 +6,7 @@ var zoomView = null;
 var panView = null;
 var maps = [];
 var initial = null;
+var onPanZoom = null;
 
 // Keep connecting to websocket
 setInterval(() => {
@@ -19,6 +20,10 @@ setInterval(() => {
 }, 1000);
 
 async function init() {
+  waitingDialog.show("Waiting for connection");
+  await setTimeout(function () {
+    waitingDialog.hide();
+  }, 30000);
   initial = await adminStartup();
   console.log(initial);
   handleMode("start-up");
@@ -63,12 +68,11 @@ async function init() {
 
   window.addEventListener("resize", function () {
     let mapWidth = Math.ceil(window.innerWidth);
-  
+
     document.getElementById("map-canvas").style.width = mapWidth * 0.5 + "px";
     document.getElementById("map-canvas").style.height = mapWidth * 0.3 + "px";
     //viewer.scaleToDimensions(2,2);
   });
-
 }
 
 // function for obt to get position to robotmarker
@@ -93,8 +97,9 @@ function registerMouseHandlers() {
       startPos.x = event.stageX;
       startPos.y = event.stageY;
       mouseDown = true;
+      onPanZoom = true;
     });
-    if (mouseDown === true) {
+    if (mouseDown === true && onPanZoom === true) {
       if (zoomKey === true) {
         var dy = event.stageY - startPos.y;
         var zoom = 1 + (10 * Math.abs(dy)) / viewer.scene.canvas.clientHeight;
@@ -109,13 +114,14 @@ function registerMouseHandlers() {
   });
 
   viewer.scene.addEventListener("stagemouseup", function (event) {
-    if (mouseDown === true) {
+    if (mouseDown === true && onPanZoom === true) {
       if (zoomKey === true) {
         zoomKey = false;
       } else if (panKey === true) {
         panKey = false;
       }
       mouseDown = false;
+      onPanZoom = false;
     }
   });
 }
@@ -124,3 +130,75 @@ async function adminStartup() {
   const response = await fetch(`http://${localhost}:8000/api/adminStartUp`);
   return response.json();
 }
+
+var waitingDialog =
+  waitingDialog ||
+  (function ($) {
+    "use strict";
+
+    // Creating modal dialog's DOM
+    var $dialog = $(
+      '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+        '<div class="modal-dialog modal-m">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
+        '<div class="modal-body">' +
+        '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+        "</div>" +
+        "</div></div></div>"
+    );
+
+    return {
+      /**
+       * Opens our dialog
+       * @param message Custom message
+       * @param options Custom options:
+       * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+       * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+       */
+      show: function (message, options) {
+        // Assigning defaults
+        if (typeof options === "undefined") {
+          options = {};
+        }
+        if (typeof message === "undefined") {
+          message = "Loading";
+        }
+        var settings = $.extend(
+          {
+            dialogSize: "m",
+            progressType: "",
+            onHide: null, // This callback runs after the dialog was hidden
+          },
+          options
+        );
+
+        // Configuring dialog
+        $dialog
+          .find(".modal-dialog")
+          .attr("class", "modal-dialog")
+          .addClass("modal-" + settings.dialogSize);
+        $dialog.find(".progress-bar").attr("class", "progress-bar");
+        if (settings.progressType) {
+          $dialog
+            .find(".progress-bar")
+            .addClass("progress-bar-" + settings.progressType);
+        }
+        $dialog.find("h3").text(message);
+        // Adding callbacks
+        if (typeof settings.onHide === "function") {
+          $dialog.off("hidden.bs.modal").on("hidden.bs.modal", function (e) {
+            settings.onHide.call($dialog);
+          });
+        }
+        // Opening dialog
+        $dialog.modal();
+      },
+      /**
+       * Closes dialog
+       */
+      hide: function () {
+        $dialog.modal("hide");
+      },
+    };
+  })(jQuery);
